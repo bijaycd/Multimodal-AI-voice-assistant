2f8e898283f6e98727ac4495ee80b7b72e787938
Bijay Chandra Das <bdas64316@gmail.com>
2025-01-03 20:01:25 +0530

    final commit

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from src.helper import voice_input, llm_model_object, text_to_speech
 
-
 def main():
+
     st.title("Multilingual AI Assistant 🤖")
     
     if st.button("Ask me anything"):
--- install_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Update and install required dependencies
-sudo apt-get update
-sudo apt-get install -y portaudio19-dev python3-dev libasound2-dev
